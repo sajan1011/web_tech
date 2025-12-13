42273ba9242077c630542bbe47dbf0473f966d5c
Sajan Shrestha <shresthasajan053@gmail.com>
2025-12-13 15:27:21 +0545

    Sorting array

--- sort.php
+++ sort.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Sorting array in Ascending and Descending order</h2>
-    <?php
+<h2>Sorting array in Ascending and Descending order</h2>
+<?php
 $arr = array(40, 12, 598, 127, 202);
 $n = count($arr);
 
@@ -41,6 +33,3 @@ for ($i = 0; $i < $n; $i++) {
     echo $arr[$i] . " ";
 }
 ?>
-
-</body>
-</html>
